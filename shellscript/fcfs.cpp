fb2b85d9795acefbf9c5fb71d22c1e34b557859f
ankingcodes <ankush.bhardwaj0@gmail.com>
2019-11-30 22:58:55 +0530

    did scheduling algo in cpp

--- shellscript/fcfs.cpp
+++ shellscript/fcfs.cpp
@@ -1,38 +1,68 @@
+//criteria - arrival time
+//Non preemptive
 #include<iostream>
 using namespace std;
-
+void print_table(int p,int p_no[], int at[], int bt[], int ct[], int tat[], int wt[]){
+  cout<<"p_no\tat\tbt\tct\ttat\twt"<<endl;
+  for(int i=0;i<p;i++){
+    cout<<p_no[i]<<"\t"<<at[i]<<"\t"<<bt[i]<<"\t"<<ct[i]<<"\t"<<tat[i]<<"\t"<<wt[i]<<endl;
+  }
+}
+void calc_ct(int p,int p_no[], int at[], int bt[], int ct[]){
+  int s = 0;
+  for(int i=0;i<p;i++){
+    //check for smallest AT
+    //if multiple AT is present,check for lowest p_no
+    s += bt[i];
+    ct[i] = s;
+  }
+}
+void calc_tat(int p, int p_no[], int ct[], int at[], int tat[]){
+  for(int i=0;i<p;i++){
+    tat[i] = ct[i] - at[i];
+  }
+}
+void calc_wt(int p, int p_no[], int tat[], int bt[], int wt[]){
+  for(int i=0;i<p;i++){
+    wt[i] = tat[i] - bt[i];
+  }
+}
+void print_avgTAT(int p, int tat[]){
+  int avg_tat = 0;
+  for(int i=0;i<p;i++){
+    avg_tat += tat[i];
+  }
+  avg_tat = avg_tat / p;
+  cout<<"Average TAT : "<<avg_tat<<endl;
+}
+void print_avgWT(int p, int wt[]){
+  int avg_wt = 0;
+  for(int i=0;i<p;i++){
+    avg_wt += wt[i];
+  }
+  avg_wt = avg_wt / 10;
+  cout<<"Average WT : "<<avg_wt<<endl;
+}
 int main(){
-  int n; cin>>n;
-  int wt[10000];
-  int bt[10000];
-  int tat[10000];
-  int avwt=0;
-  int avtat=0; int i; int j;
-  cout<<"Enter total number of processes: "<<endl;
-  //insert burst time
-  for(i=0;i<n;i++){
-    cout<<"P["<<i+1<<"]: ";
+  int p, p_no[10], ct[10], tat[10], wt[10], at[10], bt[10];
+  cout<<"Enter number of processes:";
+  cin>>p;
+  for(int i=0;i<p;i++){
+    p_no[i] = i+1;
+  }
+  cout<<"Enter burst time : "<<endl;
+  for(int i=0;i<p;i++){
     cin>>bt[i];
   }
-  wt[0]=0;
-  //cal waiting time
-  for(i=1;i<n;i++){
-    wt[i]=0;
-    for(j=0;j<i;j++)
-      wt[i]+=bt[j];
+  cout<<"Enter arrival time : "<<endl;
+  for(int i=0;i<p;i++){
+    cin>>at[i];
   }
-  cout<<"\nProcess\t\tBurst Time\tWaiting Time\tTurnaround Time";
-  //cal tat
-  for(i=0;i<n;i++){
-    tat[i]=bt[i]+wt[i];
-    avwt+=wt[i];
-    avtat+=tat[i];
-    cout<<"\nP["<<i+1<<"]"<<"\t\t"<<bt[i]<<"\t\t"<<wt[i]<<"\t\t"<<tat[i];
-  }
-  avwt/=i;
-  avtat/=i;
-  cout<<endl;
-  cout<<"Avg wt: "<<avwt<<endl;
-  cout<<"Avg tat: "<<avtat<<endl;
+  calc_ct(p,p_no,at,bt,ct);
+  calc_tat(p,p_no,ct,at,tat);
+  calc_wt(p,p_no,tat,bt,wt);
+  print_table(p,p_no,at,bt,ct,tat,wt);
+  print_avgTAT(p,tat);
+  print_avgWT(p,wt);
   return 0;
 }
--- shellscript/sjf.cpp
+++ shellscript/sjf.cpp
@@ -1,51 +1,87 @@
+//criteria - burst time
+//Non preemptive
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
+void print_table(int p,int p_no[], int at[], int bt[], int ct[], int tat[], int wt[]){
+  cout<<"p_no\tat\tbt\tct\ttat\twt"<<endl;
+  for(int i=0;i<p;i++){
+    cout<<p_no[i]<<"\t"<<at[i]<<"\t"<<bt[i]<<"\t"<<ct[i]<<"\t"<<tat[i]<<"\t"<<wt[i]<<endl;
+  }
+}
+void calc_tat(int p, int p_no[], int ct[], int at[], int tat[]){
+  for(int i=0;i<p;i++){
+    tat[i] = ct[i] - at[i];
+  }
+}
+void calc_wt(int p, int p_no[], int tat[], int bt[], int wt[]){
+  for(int i=0;i<p;i++){
+    wt[i] = tat[i] - bt[i];
+  }
+}
+void calc_ct(int p, int p_no[], int at[], int bt[], int ct[]){
+  int s = 0;
+  int dt[10];
+  for(int i=0;i<p;i++){
+    dt[i] = bt[i];
+  }
+  if(at[0]!=0){
+      s = s + at[0];
+  }
+  s = s + bt[0];
+  ct[0] = s;
+  //find min bt and add it to s 
+  for(int i=1;i<p;i++){
+    int min = INT_MAX;
+    int pos;
+    for(int j=0;j<p;j++){
+      if(dt[j]<min){
+        min = dt[j];
+        pos = j;
+      }
+    }
+    s += bt[pos];
+    ct[i] = s;
+    dt[pos] = INT_MAX;
+  }
+}
+void print_avgTAT(int p, int tat[]){
+  int avg_tat = 0;
+  for(int i=0;i<p;i++){
+    avg_tat += tat[i];
+  }
+  avg_tat = avg_tat / p;
+  cout<<"Average TAT : "<<avg_tat<<endl;
+}
+void print_avgWT(int p, int wt[]){
+  int avg_wt = 0;
+  for(int i=0;i<p;i++){
+    avg_wt += wt[i];
+  }
+  avg_wt = avg_wt / 10;
+  cout<<"Average WT : "<<avg_wt<<endl;
+}
 int main(){
-  int bt[20],p[20],wt[20],tat[20],i,j,n,total=0,pos,temp;
-  float avg_wt, avg_tat;
-  printf("Enter number of processes: ");
-  scanf("%d",&n);
-  printf("Enter burst time: \n");
-  for(i=0;i<n;i++){
-    printf("p%d:",i+1);
-    scanf("%d",&bt[i]);
-    p[i]=i+1;
+  int p, p_no[10], ct[10], tat[10], wt[10], at[10], bt[10];
+  cout<<"Enter number of processes:";
+  cin>>p;
+  for(int i=0;i<p;i++){
+    p_no[i] = i+1;
   }
-  //sorting bt in ascending order
-  // for(i=0;i<n;i++){
-  //   pos=i;
-  //   for(j=i+1;j<n;j++){
-  //     if(bt[j]<bt[pos])
-  //     pos=j;
-  //   }
-  //   temp=bt[i];
-  //   bt[i]=bt[pos];
-  //   bt[pos]=temp;
-  //
-  //   temp=p[i];
-  //   p[i]=p[pos];
-  //   p[pos]=temp;
-  // }
-  sort(bt,bt+n);
-  sort(p,p+n);
-  wt[0]=0;
-  //cal waiting time
-  for(i=1;i<n;i++){
-    wt[i]=0;
-    for(j=0;j<i;j++)
-    wt[i]+=bt[j];
-    total+=wt[i];
+  cout<<"Enter burst time : "<<endl;
+  for(int i=0;i<p;i++){
+    cin>>bt[i];
   }
-  avg_wt=(float)total/n;
-  total=0;
-  printf("\nProcess\t\tBurstTime\tWaitingTime\tTurnaroundTime");
-  for(i=0;i<n;i++){
-    tat[i]=bt[i]+wt[i];
-    total+=tat[i];
-    printf("\n%d\t\t%d\t\t%d\t\t%d",p[i],bt[i],wt[i],tat[i]);
+  cout<<"Enter arrival time : "<<endl;
+  for(int i=0;i<p;i++){
+    cin>>at[i];
   }
-  avg_tat=(float)total/n;
-  printf("\nAvg_wt=%f",avg_wt);
-  printf("\nAvg_tat=%f",avg_tat);
+
+  calc_ct(p,p_no,at,bt,ct); 
+  calc_tat(p,p_no,ct,at,tat);
+  calc_wt(p,p_no,tat,bt,wt);
+  print_table(p,p_no,at,bt,ct,tat,wt);
+  print_avgTAT(p,tat);
+  print_avgWT(p,wt);
+  return 0;
 }
